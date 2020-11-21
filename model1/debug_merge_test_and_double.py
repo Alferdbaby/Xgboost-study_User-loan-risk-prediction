f956a10495f19fd7e3b33336f85799fbfa2281b7
unknown <zxf19930117@163.com>
2020-11-21 11:59:35 +0800

    debug merge

---
+++ model1/debug_merge_test_and_double.py
@@ -0,0 +1,5 @@
+import pandas as pd
+test_data = pd.read_csv("E:\\git_project\\Xgboost-study_User-loan-risk-prediction\\model1\\test_data.csv")
+test_data.head(5)
+is_double_ = pd.read_csv("E:\\git_project\\Xgboost-study_User-loan-risk-prediction\\model1\\is_double_.csv")
+is_double_.head(5)
